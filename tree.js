2802519119e237a4bac589d2f6a4b311f78008c4
Gradnos <sdolaqidze@gmail.com>
2023-04-09 14:27:11 +0400

    Create a Tree class + insert function

--- tree.js
+++ tree.js
@@ -5,3 +5,51 @@ class Node{
         this.right = null;
     }
 }
+
+class Tree{
+
+    buildTree(arr){
+
+
+
+    }
+
+    insert(value){
+        if(!this.root) return this.root = new Node(value);
+        
+        let current = this.root;
+
+
+        while(true){
+            if(value > current.value){
+                if(current.right === null) return current.right = new Node(value);
+                current = current.right;
+            } else if(value < current.value){
+                if(current.left === null) return current.left = new Node(value);
+                current = current.left;
+            } if(current.value === value) return;
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+}
+
+let tree = new Tree();
+
+console.log(tree.insert(5));
+console.log(tree.insert(5));
+console.log(tree.insert(2));
+console.log(tree.insert(1));
+console.log(tree.insert(3));
+console.log(tree.insert(9));
+console.log(tree.insert(10));
+console.log(tree);
